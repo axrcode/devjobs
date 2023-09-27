13f172e184f415bc770640da53fc0fc7663e3cb6
axrcode <axrcode@gmail.com>
2023-09-27 09:33:00 -0600

    Change - Diseño de login y registro

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,42 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('¿Olvidaste tu contraseña?. Coloca tu email de registro y te enviaremos un enlace para que puedas crea uno nuevo.') }}
     </div>
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
+    <form method="POST" action="{{ route('password.email') }}" novalidate>
         @csrf
 
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+
+            <x-text-input   id="email"
+                            class="block mt-1 w-full {{ $errors->get('email') ? 'border-red-600' : '' }}"
+                            type="email"
+                            name="email"
+                            :value="old('email')"
+                            required
+                            autofocus
+            />
+            
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <x-primary-button class="w-full justify-center my-5">
+            {{ __('Enviar Email') }}
+        </x-primary-button>
+
+        <div class="flex justify-between">
+            <x-link :href="route('login')">
+                Iniciar Sesión
+            </x-link>
+
+            <x-link :href="route('register')">
+                Registrarse
+            </x-link>
         </div>
     </form>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,37 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" novalidate>
         @csrf
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-label for="name" :value="__('Nombre')" />
+
+            <x-text-input   id="name"
+                            class="block mt-1 w-full {{ $errors->get('name') ? 'border-red-600' : '' }}"
+                            type="text"
+                            name="name"
+                            autocomplete="name"
+                            :value="old('name')"
+                            required
+                            autofocus
+            />
+
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+
+            <x-text-input   id="email"
+                            class="block mt-1 w-full {{ $errors->get('email') ? 'border-red-600' : '' }}"
+                            type="email"
+                            name="email"
+                            autocomplete="username"
+                            :value="old('email')"
+                            required
+            />
+
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -20,33 +39,44 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input   id="password" 
+                            class="block mt-1 w-full {{ $errors->get('password') ? 'border-red-600' : '' }}"
                             type="password"
                             name="password"
-                            required autocomplete="new-password" />
+                            autocomplete="new-password"
+                            required
+            />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirmar Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+            <x-text-input   id="password_confirmation"
+                            class="block mt-1 w-full {{ $errors->get('password_confirmation') ? 'border-red-600' : '' }}"
                             type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                            name="password_confirmation"
+                            autocomplete="new-password"
+                            required
+            />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+        <x-primary-button class="w-full justify-center my-5">
+            {{ __('Registrarse') }}
+        </x-primary-button>
 
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <div class="flex justify-between">
+            <x-link :href="route('login')">
+                Iniciar Sesión
+            </x-link>
+
+            <x-link :href="route('password.request')">
+                Olvidate tu password
+            </x-link>
         </div>
     </form>
 </x-guest-layout>
